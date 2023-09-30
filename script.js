ebdbb88e6a931e01092cc853a6bea03a48798472
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-30 12:50:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function allSizes () {
+const size1 = document.getElementById("size");
+const widthSize = window.innerWidth;
+const heigthSize = window.innerHeight;
+
+size1.textContent = `width: ${widthSize} | height: ${heigthSize}`
+}
+allSizes();
+
+
+setInterval(allSizes);
